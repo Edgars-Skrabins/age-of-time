8481eeb975756949165ad6cdc2a284b8b72ec97c
Edgars Skrabiņš <shiftkiro@gmail.com>
2025-06-21 20:18:39 +0300

    Card generation logic complete

--- Assets/Scripts/UpgradeCardGenerator.cs
+++ Assets/Scripts/UpgradeCardGenerator.cs
@@ -1,15 +1,54 @@
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class UpgradeCardGenerator : MonoBehaviour
 {
     [SerializeField] private int m_amountOfCardsToSpawn;
+    [SerializeField] private float m_generationRate;
     [SerializeField] private UpgradeCard m_upgradeCardPrefab;
     [SerializeField] private UpgradeCardSO[] m_upgradeCardSOs;
-    private UpgradeCard[] m_spawnedUpgradeCards;
+
+    private List<UpgradeCard> m_spawnedUpgradeCards;
+    private float m_generationTimer;
+
+    private void Update()
+    {
+        if (ShouldCountGenerationTimer())
+        {
+            CountGenerationTimer();
+        }
+
+        if (ShouldGenerateCards())
+        {
+            GenerateCards();
+        }
+    }
+
+    private bool ShouldGenerateCards()
+    {
+        return m_generationTimer >= m_generationRate && m_spawnedUpgradeCards.Count < m_amountOfCardsToSpawn;
+    }
+
+    private bool ShouldCountGenerationTimer()
+    {
+        return m_generationTimer < m_generationRate;
+    }
+
+    private void CountGenerationTimer()
+    {
+        m_generationTimer += Time.deltaTime;
+    }
+
+    private void ResetGenerationTimer()
+    {
+        m_generationTimer = 0f;
+    }
 
     private void HandleCardPicked()
     {
         DeleteAllSpawnedCards();
+        ResetGenerationTimer();
     }
 
     private void DeleteAllSpawnedCards()
@@ -18,6 +57,7 @@ public class UpgradeCardGenerator : MonoBehaviour
         {
             Destroy(card.gameObject);
         }
+        m_spawnedUpgradeCards.Clear();
     }
 
     private void GenerateCards()
@@ -26,6 +66,7 @@ public class UpgradeCardGenerator : MonoBehaviour
         {
             UpgradeCardSO upgradeCardSO = m_upgradeCardSOs[Random.Range(0, m_upgradeCardSOs.Length)];
             UpgradeCard upgradeCard = Instantiate(m_upgradeCardPrefab, transform);
+            m_spawnedUpgradeCards.Add(upgradeCard);
             upgradeCard.SetButtonImage(upgradeCardSO.m_sprite);
             upgradeCard.OnButtonClick += HandleCardPicked;
         }
