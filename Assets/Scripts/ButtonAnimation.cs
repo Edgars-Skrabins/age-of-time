dae11d9abf289d50f0e66a3891a917cfde2f152d
indcrediBro <chirag.agarwal8393@gmail.com>
2025-06-25 18:14:26 +0530

    Added UI Button Animations

---
+++ Assets/Scripts/ButtonAnimation.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonAnimation : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private float shakeMagnitude = 10f;
+    [SerializeField] private float shakeSpeed = 50f;
+
+    private RectTransform rectTransform;
+    private Vector3 originalPosition;
+    private Coroutine shakeRoutine;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        originalPosition = rectTransform.anchoredPosition;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (shakeRoutine == null)
+            shakeRoutine = StartCoroutine(ShakeLoop());
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (shakeRoutine != null)
+        {
+            StopCoroutine(shakeRoutine);
+            shakeRoutine = null;
+            rectTransform.anchoredPosition = originalPosition;
+        }
+    }
+
+    private IEnumerator ShakeLoop()
+    {
+        while (true)
+        {
+            float offsetX = Mathf.PerlinNoise(Time.unscaledTime * shakeSpeed, 0f) * 2f - 1f;
+            float offsetY = Mathf.PerlinNoise(0f, Time.unscaledTime * shakeSpeed) * 2f - 1f;
+
+            Vector3 shakeOffset = new Vector3(offsetX, offsetY, 0f) * shakeMagnitude;
+            rectTransform.anchoredPosition = originalPosition + shakeOffset;
+
+            yield return null;
+        }
+    }
+}
