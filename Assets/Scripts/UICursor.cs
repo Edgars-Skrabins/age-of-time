79c1c0abed07616717430719c0d63b091e994721
indcrediBro <chirag.agarwal8393@gmail.com>
2025-06-26 10:03:44 +0530

    Added UI Cursor and Removed Unity Cursor With Animations

---
+++ Assets/Scripts/UICursor.cs
@@ -0,0 +1,115 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
+public class UICursor : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private RectTransform m_cursorRect;
+    [SerializeField] private Canvas m_canvas;
+    [SerializeField] private Animator m_cursorAnimator;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
+
+        DetectClick();
+        FollowMouse();
+        DetectHover();
+    }
+
+    private void FollowMouse()
+    {
+        Vector2 mousePos = Input.mousePosition;
+
+        if (m_canvas.renderMode == RenderMode.ScreenSpaceOverlay)
+        {
+            m_cursorRect.position = mousePos;
+        }
+        else
+        {
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                m_canvas.transform as RectTransform,
+                mousePos,
+                m_canvas.worldCamera,
+                out Vector2 localPos);
+
+            m_cursorRect.localPosition = localPos;
+        }
+    }
+
+    private void DetectHover()
+    {
+        PointerEventData data = new PointerEventData(EventSystem.current)
+        {
+            position = Input.mousePosition
+        };
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(data, results);
+
+        bool isHovering = false;
+
+        foreach (RaycastResult result in results)
+        {
+            GameObject go = result.gameObject;
+
+            if (
+                go.GetComponent<Button>() ||
+                go.GetComponent<Toggle>() ||
+                go.GetComponent<Slider>() ||
+                go.GetComponent<Enemy>() ||
+                go.GetComponentInParent<Enemy>()
+            )
+            {
+                isHovering = true;
+                break;
+            }
+        }
+
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPosition.z = 0f;
+
+        RaycastHit2D hit = Physics2D.Raycast(mouseWorldPosition, Vector2.zero);
+        if (hit.collider != null)
+        {
+            Enemy enemy = hit.collider.GetComponentInParent<Enemy>();
+            if (enemy != null)
+            {
+                isHovering = true;
+            }
+        }
+
+        m_cursorAnimator.SetBool("Hover", isHovering);
+    }
+
+
+    private GameObject GetHoveredUIObject()
+    {
+        PointerEventData data = new PointerEventData(EventSystem.current)
+        {
+            position = Input.mousePosition
+        };
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(data, results);
+
+        return results.Count > 0 ? results[0].gameObject : null;
+    }
+
+    private void DetectClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            m_cursorAnimator.SetTrigger("Click");
+        }
+    }
+}
